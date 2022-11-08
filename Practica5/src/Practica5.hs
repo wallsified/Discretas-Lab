c71ed72b2bd585c35493e58b1304f3945235d51f
Wallsified <danielparedes@ciencias.unam.mx>
2022-11-07 21:45:47 -0600

    Practica #5 Start

--- Practica5/src/Practica5.hs
+++ Practica5/src/Practica5.hs
@@ -5,3 +5,46 @@ Número de cuenta: 320140655
 Oyente: Paredes Zamudio Luis Daniel
 Número de Cuenta: 318159926
 -}
+
+--Alias para variables
+type Nombre = String
+
+--Tipo para las proposiciones
+data LProp = T | F | VarP Nombre | Conj Lprop LProp | Disy LProp LProp | Impl LProp LProp | Syss LProp LProp | Neg LProp deriving (Show, Eq)
+
+instance Show LProp where
+    show (Var a)    = show a
+    show PFalse     = show False
+    show PTrue      = show True
+    show (Neg a)    = "¬" ++ show a
+    show (Conj a b) = "(" ++ show a ++ " ^ " ++ show b ++ ")"
+    show (Disy a b) = "(" ++ show a ++ " v " ++ show b ++ ")"
+    show (Impl a b) = "(" ++ show a ++ " -> " ++ show b ++ ")"
+    show (Syss a b) = "(" ++ show a ++ " <-> " ++ show b ++ ")"
+
+
+--Tipo para el Tableaux
+data Tableaux = Hoja [LProp] | Alpha [LProp] Tableaux | Beta [LProp] Tableaux Tableaux deriving (Show, Eq)
+
+{- instance Show Tableaux where
+    show (Hoja [x] ) =
+    show (Alpha [x] a) =
+    show (Beta [x] a b) = -}
+
+-- 1. literales Función que nos dice si en una lista de fórmulas, todas son literales.
+
+-- 2. nextF Función que regresa la primera fórmula que no es literal de una lista de fórmulas.
+
+-- 3. alpha Nos dice si una fórmula f es una fórmula α
+
+-- 4. beta  Nos dice si una fórmula f es una fórmula β
+
+-- 5. sigma Nos dice si una fórmula f es una fórmula σ
+
+-- 6. expAlpha Dada una lista de fórmulas l y una fórmula f realiza la expansión alpha de f dentro la lista l.
+
+-- 7. expBeta  Dada una lista de fórmulas l y una fórmula f realiza la expansión beta de f sobre la lista l.
+
+-- 8. expSigma Dada una lista de fórmulas l y una fórmula f , realiza la expansión sigma de f sobre la lista l.
+
+-- 9. consTableaux Construye el tableau a partir de una fórmula.
